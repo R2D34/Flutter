99b076afdeade7521702985c6c00731de27db656
Aleksander Żołądkiewicz <alxzoladkiewicz@gmail.com>
2019-07-16 23:57:37 +0200

    Refactoring ProductList logic into the method
    Swapping terenary expression from build method to the separate, more readable method.

--- first_app/lib/products.dart
+++ first_app/lib/products.dart
@@ -1,30 +1,41 @@
 import 'package:flutter/material.dart';
 
-class Products extends StatelessWidget{
+class Products extends StatelessWidget {
   final List<String> products;
 
-  Products(this.products){
+  Products(this.products) {
     print('Products widget constructor');
   }
 
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset(
+            'assets/Pixel_Night_Town.gif',
+          ),
+          Text(products[index]),
+        ],
+      ),
+    );
+  }
 
-Widget _buildProductItem(BuildContext context, int index){
-  return Card(
-                      child: Column(
-                        children: <Widget>[
-                          Image.asset('assets/Pixel_Night_Town.gif',),
-                          Text(products[index]),
-                        ],
-                      ),
-                    );
-                  
-}
+  Widget _buildProductList() {
+    print('Products build method');
+    Widget productCard;
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCard = Center(child: Text('No Dragons in this lair'));
+    }
+    return productCard;
+  }
+
   @override
   Widget build(BuildContext context) {
-    print('Products build method');
-    return products.length > 0 ? ListView.builder(
-              itemBuilder: _buildProductItem,
-              itemCount: products.length,
-              ) : Center(child: Text('No Dragons in this lair'));
+    return _buildProductList();
   }
 }
