6d54e0fc721f1466d5d51f9862e2fb12348a16ba
Aleksander Żołądkiewicz <alxzoladkiewicz@gmail.com>
2019-07-17 22:38:04 +0200

    Adding Tabs to ProductManager

--- first_app/lib/pages/manage_products.dart
+++ first_app/lib/pages/manage_products.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/pages/products.dart';
+import 'package:first_app/pages/product_create.dart';
+import 'package:first_app/pages/product_list.dart';
+
 class ManageProducts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Manager'),
-      ),
-      body: Center(
-        child: Text('This is your Dragon tamer'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
+              ListTile(
+                title: Text('Home'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => ProductsPage()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('Product Manager'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.dashboard), text: 'Summon a dragon'),
+              Tab(icon: Icon(Icons.line_style), text: 'Dragon\'s Stable'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            ProductCreatePage(),
+            ProductListPage(),
+          ],
+        ),
       ),
     );
   }
