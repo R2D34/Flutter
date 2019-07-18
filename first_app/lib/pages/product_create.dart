aa32fbd672e0159a345080ddc68edcf1b497891d
Aleksander Żołądkiewicz <alxzoladkiewicz@gmail.com>
2019-07-18 18:02:37 +0200

    Added modal message.

--- first_app/lib/pages/product_create.dart
+++ first_app/lib/pages/product_create.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget{
+class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Create Product'),);
+    return Center(
+      child: RaisedButton(
+        child: Text('Fus ro dah!'),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return Center(
+                  child: Text('Here comes the Dragonborn\' Spell'),
+                );
+              });
+        },
+      ),
+    );
   }
 }
