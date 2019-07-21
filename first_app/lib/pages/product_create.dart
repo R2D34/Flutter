0e232687a11d86b026863d5e8f702a200d83a372
Aleksander Żołądkiewicz <alxzoladkiewicz@gmail.com>
2019-07-21 23:49:18 +0200

    Adding input forms to create product

--- first_app/lib/pages/product_create.dart
+++ first_app/lib/pages/product_create.dart
@@ -1,21 +1,43 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue = '';
+  String descriptionValue = '';
+  double priceValue;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Fus ro dah!'),
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Center(
-                  child: Text('Here comes the Dragonborn\' Spell'),
-                );
-              });
-        },
-      ),
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              titleValue = value;
+            });
+          },
+        ),
+        TextField(maxLines: 4,
+          onChanged: (String value) {
+            setState(() {
+              descriptionValue = value;
+            });
+          },
+        ),
+        TextField(keyboardType: TextInputType.number,
+          onChanged: (String value) {
+            setState(() {
+              priceValue = double.parse(value);
+            });
+          },
+        ),
+      ],
     );
   }
 }
