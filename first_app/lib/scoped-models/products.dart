fc5d3b709597dc6f0a26cc4157128c0d23f66e72
Aleksander Żołądkiewicz <alxzoladkiewicz@gmail.com>
2019-08-14 15:11:35 +0200

    ToggleFavorite Function

--- first_app/lib/scoped-models/products.dart
+++ first_app/lib/scoped-models/products.dart
@@ -6,37 +6,50 @@ class ProductsModel extends Model {
   List<Product> _products = [];
   int _selectedProductIndex;
 
-List<Product> get products{
-  return List.from(_products);
-}
-int get selectedProductIndex {
-  return _selectedProductIndex;
-}
-
-Product get selectedProduct {
-  if(_selectedProductIndex == null){
-    return null;
+  List<Product> get products {
+    return List.from(_products);
   }
-  return _products[_selectedProductIndex];
-}
+
+  int get selectedProductIndex {
+    return _selectedProductIndex;
+  }
+
+  Product get selectedProduct {
+    if (_selectedProductIndex == null) {
+      return null;
+    }
+    return _products[_selectedProductIndex];
+  }
 
   void addProduct(Product product) {
-      _products.add(product);
-      _selectedProductIndex = null;
+    _products.add(product);
+    _selectedProductIndex = null;
   }
 
   void deleteProduct() {
-      _products.removeAt(_selectedProductIndex);
-      _selectedProductIndex = null;
+    _products.removeAt(_selectedProductIndex);
+    _selectedProductIndex = null;
   }
 
-  void updateProduct(Product product){
-     _products[_selectedProductIndex] = product; 
-     _selectedProductIndex = null;
+  void updateProduct(Product product) {
+    _products[_selectedProductIndex] = product;
+    _selectedProductIndex = null;
   }
 
-  void selectProduct(int index){
+  void toggleProductFavoriteStatus() {
+    final bool isCurrentlyFavorite = selectedProduct.isFavorite;
+    final bool newFavoriteStatus = !isCurrentlyFavorite;
+    final Product updatedProduct = Product(
+        title: selectedProduct.title,
+        description: selectedProduct.description,
+        price: selectedProduct.price,
+        image: selectedProduct.image,
+        isFavorite: newFavoriteStatus);
+        _products[_selectedProductIndex] = updatedProduct;
+        _selectedProductIndex = null;
+  }
+
+  void selectProduct(int index) {
     _selectedProductIndex = index;
   }
-  
 }
--- first_app/lib/widgets/products/product_card.dart
+++ first_app/lib/widgets/products/product_card.dart
@@ -1,9 +1,12 @@
 import 'package:first_app/widgets/products/address_tag.dart';
 import 'package:flutter/material.dart';
 
+import 'package:scoped_model/scoped_model.dart';
+
 import '../ui_elements/title_default.dart';
 import './price_tag.dart';
 import '../../models/product.dart';
+import '../../scoped-models/products.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
@@ -25,27 +28,32 @@ class ProductCard extends StatelessWidget {
     );
   }
 
-_buildActionButtons(BuildContext context){
-  return           ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.info),
-                iconSize: 50.0,
-                color: Theme.of(context).primaryColor,
-                onPressed: () => Navigator.pushNamed<bool>(
-                    context, '/product/' + productIndex.toString()),
-              ),
-              IconButton(
-                icon: Icon(Icons.favorite_border),
-                iconSize: 50.0,
-                color: Colors.red,
-                onPressed: () => Navigator.pushNamed<bool>(
-                    context, '/product/' + productIndex.toString()),
-              )
-            ],
+  _buildActionButtons(BuildContext context) {
+    return ButtonBar(
+      alignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(Icons.info),
+          iconSize: 50.0,
+          color: Theme.of(context).primaryColor,
+          onPressed: () => Navigator.pushNamed<bool>(
+              context, '/product/' + productIndex.toString()),
+        ),
+        ScopedModelDescendant<ProductsModel>(
+            builder: (BuildContext context, Widget child, ProductsModel model) {
+          return IconButton(
+            icon: Icon(model.products[productIndex].isFavorite ? Icons.favorite : Icons.favorite_border),
+            iconSize: 50.0,
+            color: Colors.red,
+            onPressed: () {
+              model.selectProduct(productIndex);
+              model.toggleProductFavoriteStatus();
+            },
           );
-}
+        })
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
