fc4237b24838919eb27ea722c9de961778ebc721
Aleksander Żołądkiewicz <alxzoladkiewicz@gmail.com>
2019-08-20 10:29:03 +0200

    Added display filtration with favorite toggle

--- first_app/lib/pages/products.dart
+++ first_app/lib/pages/products.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'package:scoped_model/scoped_model.dart';
+
+import '../scoped-models/products.dart';
 import '../widgets/products/products.dart';
 
-
 class ProductsPage extends StatelessWidget {
-
-Widget _buildSideDrawer(BuildContext context) {
-  return Drawer(
-        child: Column(
-          children: <Widget>[
-            AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
-            ListTile(
-              leading: Icon(Icons.edit),
-              title: Text('Tame your dragon!'),
-              onTap: () {
-                Navigator.pushReplacementNamed(context, '/admin');
-              },
-            ),
-          ],
-        ),
-      );
-}
+  Widget _buildSideDrawer(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Tame your dragon!'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/admin');
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,9 +31,17 @@ Widget _buildSideDrawer(BuildContext context) {
       appBar: AppBar(
         title: Text('List'),
         actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
+          ScopedModelDescendant<ProductsModel>(
+            builder: (BuildContext context, Widget child, ProductsModel model) {
+              return IconButton(
+                icon: Icon(model.displayFavoritesOnly
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                onPressed: () {
+                  model.toggleDisplayMode();
+                },
+              );
+            },
           )
         ],
       ),
