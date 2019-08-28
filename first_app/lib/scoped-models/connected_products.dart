44b9dcae5c2b507d59efbcd0616cbb0ccbbf7714
Aleksander Żołądkiewicz <alxzoladkiewicz@gmail.com>
2019-08-28 13:54:03 +0200

    Made deletion method work for the product on the server

--- first_app/lib/scoped-models/connected_products.dart
+++ first_app/lib/scoped-models/connected_products.dart
@@ -80,8 +80,15 @@ mixin ProductsModel on ConnectedProductsModel {
   }
 
   void deleteProduct() {
+    _isLoading = true;
+    final deletedProductId = selectedProduct.id;
     _products.removeAt(selectedProductIndex);
+    _selProductIndex = null;
     notifyListeners();
+    http.delete('https://dojo-1.firebaseio.com/products/${deletedProductId}.json').then((http.Response response) {
+      _isLoading = false;
+      notifyListeners();
+    });
   }
 
   void fetchProducts() {
