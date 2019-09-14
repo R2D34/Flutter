a20b758afb0354fadbc2e7b9154625a3e07cf357
Aleksander <alxzoladkiewicz@gmail.com>
2019-09-14 15:51:49 +0200

    Revert "Editing only for the user who created the product"
    This reverts commit c23d8336c2894e75498be699393ef560b1ec73bc.

--- first_app/lib/scoped-models/connected_products.dart
+++ first_app/lib/scoped-models/connected_products.dart
@@ -77,7 +77,7 @@ mixin ProductsModel on ConnectedProductsModel {
     });
   }
 
-  Future<Null> fetchProducts({onlyForUser = false}) {
+  Future<Null> fetchProducts() {
     print('Loading Products');
     _isLoading = true;
     notifyListeners();
@@ -102,15 +102,10 @@ mixin ProductsModel on ConnectedProductsModel {
             price: productData['price'],
             userEmail: productData['userEmail'],
             userId: productData['userId'],
-            isFavorite: productData['wishlistUsers'] == null
-                ? false
-                : (productData['wishlistUsers'] as Map<String, dynamic>)
-                    .containsKey(_authenticatedUser.id));
+            isFavorite: productData['wishlistUsers'] == null ? false : (productData['wishlistUsers'] as Map<String, dynamic>).containsKey(_authenticatedUser.id));
         fetchedProductList.add(product);
       });
-      _products = onlyForUser ? fetchedProductList.where((Product product) {
-        return product.userId == _authenticatedUser.id;
-      }).toList() : fetchedProductList;
+      _products = fetchedProductList;
       _isLoading = false;
       print('Finished loading products');
 
